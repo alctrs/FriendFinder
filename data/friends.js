0a65c9f4d99df003e4486c7d754440ab8af433b8
alctrs <mgamiddo@gmail.com>
2018-05-25 10:30:49 -0500

    friends edit

--- data/friends.js
+++ data/friends.js
@@ -33,8 +33,8 @@ var friendArray = [{
   },
 
   {
-    "name":"Tweety",
-    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjCaLon7D69ny66npkB8ApSD6b9l_P6A-YPRQl6NBXiFMA-4lSEQ",
+    "name":"Captian America",
+    "photo":"https://i2.wp.com/marvelstudiosnews.com/wp-content/uploads/2018/04/30629684_10155415196527344_8867921423906111488_n.jpg?fit=639%2C960&quality=85&strip=info&ssl=1&w=657&h=505",
     "scores":[
         2,
         3,
@@ -49,8 +49,8 @@ var friendArray = [{
       ]
   },
   {
-    "name":"Tweety",
-    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjCaLon7D69ny66npkB8ApSD6b9l_P6A-YPRQl6NBXiFMA-4lSEQ",
+    "name":"Dory",
+    "photo":"https://d.ibtimes.co.uk/en/full/1455968/finding-dory-movie.jpg",
     "scores":[
         2,
         3,
